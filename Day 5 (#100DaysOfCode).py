fde61a4e3ea03fb9e085df9fa6c9068a035c22da
Timirichy <97792851+Timirichy@users.noreply.github.com>
2022-03-20 23:14:49 +0100

    Add files via upload
    day 11 #100daysofcode

--- Day 5 (#100DaysOfCode).py
+++ Day 5 (#100DaysOfCode).py
@@ -1,10 +1,23 @@
-# LOOPS
+                    # LOOPS
 
-# 049
-# for loop
+                    # 049
+                    # for loop
 
 fruits = ['Apple', 'Peach', 'Pear']
 for fruit in fruits:
     print(fruit)
     print(fruit + "Pie")
 print(fruits)
+
+                    # 052
+                    # for loop with range
+for number in range(1, 11, 3):
+    # 3 is for how many times it jumps before going to the next number
+    #  it starts printing from 1 till 10
+    print(number)   
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+
